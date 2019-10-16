4ec059dcbd4ead8e1af1c66d60d50d3e311028dc
James Chalk <jmmchalk@gmail.com>
2019-10-16 17:05:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,6 +11,17 @@ return "You are a teenager!";
 }
 
 function ageChecker(age) {
+  if (age>=13 && age <= 19){
+return "You are a teenager!";
+} else if(age <=12){
+  return "You are a kid";
+} else {return "You are grownup";}
+  
+}
+  
+}
+}
+  
 
 }
 
